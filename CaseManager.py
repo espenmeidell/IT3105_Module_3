151cb03ebf76f3a0bfcf270ea49f2a4942d0bd3e
Espen Meidell <espen.meidell@gmail.com>
2017-10-04 11:41:47 +0200

    Case Manager works

--- CaseManager.py
+++ CaseManager.py
@@ -1,6 +1,7 @@
 from typing import Callable, List, Any
 import numpy as np
 
+
 Case = List[Any]
 
 
@@ -15,5 +16,18 @@ class CaseManager:
 
         self.cases = np.array(case_function())
         np.random.shuffle(self.cases)
-        
+        separator1 = round(len(self.cases) * self.training_fraction)
+        separator2 = separator1 + round(len(self.cases) * self.validation_fraction)
+        self.training_cases = self.cases[0:separator1]
+        self.validation_cases = self.cases[separator1:separator2]
+        self.testing_cases = self.cases[separator2:]
+
+    def get_training_cases(self): return self.training_cases
+
+    def get_validation_cases(self): return self.validation_cases
+
+    def get_testing_cases(self): return self.testing_cases
+
+
+
 
